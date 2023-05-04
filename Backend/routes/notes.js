d7ba35a8266cb7c4c7479be88a4e4cab3da4a3e2
JassiSingh08 <singhjassi693@gmail.com>
2023-05-04 15:38:07 +0530

    adding note and fetch all note from user

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,10 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-
-  res.json([])
-
+// ROUTE 1: Get all the notes using: GET "./api/auth/fetchallnotes. " Login required.
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
+
+// ROUTE 2: Add a new Note using: POST  "./api/auth/addnote. " Login required.
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters long").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      //if there are errors show status 400
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).jsom({ error: errors.array() });
+      }
+
+      //Destructuring taking data from note schema
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 
 module.exports = router;
