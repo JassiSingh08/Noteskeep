78d66051205f2eb6013b73b203cbf0f7e8237653
JassiSingh08 <singhjassi693@gmail.com>
2023-05-11 13:49:30 +0530

    About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <figure>
+      <blockquote class="blockquote">
+        <p>
+          Welcome to iNotebook web app!
+          <br />
+          Our app is designed to make it easy for you to store and access your
+          notes from anywhere, at any time. <br /> With our app, you no longer
+          have to worry about losing your notes or forgetting where you saved
+          them. <br />
+          All your notes are securely stored in the cloud, so you can access
+          them from any device with an internet connection. Our app features a
+          simple and intuitive interface, making it easy for you to create and
+          organize your notes.
+          <br />
+          You can create notes on any topic, add tags to help you find them
+          later, and even share them with others. One of the key benefits of our
+          app is its flexibility. Whether you're at home, at work, or on the go,
+          you can access your notes from any device with an internet connection.
+          This means you can work on your notes wherever and whenever
+          inspiration strikes. <br />
+          <br />
+          Thank you for choosing iNotebook web app. We hope you find it useful
+          and enjoyable to use!
+        </p>
+      </blockquote>
+      <figcaption class="blockquote-footer">
+       <cite title="Source Title">iNotebook</cite>
+      </figcaption>
+    </figure>
+  );
+};
 
-export default About
+export default About;
